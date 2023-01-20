c60ce41501a44f9538bc9a90ea0e121938595c3e
SriGanez.d <sriganesh.d@i-exceed.com>
2023-01-20 14:49:47 +0530

    changes

--- src/test/java/GridRunner1.java
+++ src/test/java/GridRunner1.java
@@ -5,22 +5,21 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
 
-import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.URL;
 
-public class GridRunner extends TestBase{
+public class GridRunner1 extends TestBase{
 
     public WebDriver driver;
 
 
-    public void chrome() {
+
+    public void firefox() {
         DesiredCapabilities caps = new DesiredCapabilities();
-        caps.setBrowserName(BrowserType.CHROME);
+        caps.setBrowserName(BrowserType.FIREFOX);
         try {
             driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), caps);
-            driver.get("http://www.google.com");
-            Thread.sleep(20000);
+            driver.get("http://www.youtube.com");
+            Thread.sleep(40000);
             driver.manage().window().fullscreen();
             System.out.println(" From Remote " + driver.getTitle());
         }catch (Exception e){
@@ -31,18 +30,13 @@ public class GridRunner extends TestBase{
     }
 
     @Test
-    public void invokeChrome() throws Exception {
-        Thread.sleep(4000);
-        TestBase.initialization("chrome");
-        TestBase.getDriver().get("http://www.google.com");
+    public void invokeFire() throws Exception {
+        Thread.sleep(8000);
+        TestBase.initialization("firefox");
+        TestBase.getDriver().get("http://www.youtube.com");
         Thread.sleep(20000);
         TestBase.getDriver().quit();
     }
-
-    public static void main(String[] args) throws IOException {
-        Runtime.getRuntime().exec("docker-compose -f docker-compose-v3-video.yml up");
-    }
-
 
 
 }
--- src/test/java/GridRunner2.java
+++ src/test/java/GridRunner2.java
@@ -5,21 +5,19 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
 
-import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.URL;
 
-public class GridRunner extends TestBase{
+public class GridRunner2 extends TestBase {
 
     public WebDriver driver;
 
 
-    public void chrome() {
+    public  void edge() {
         DesiredCapabilities caps = new DesiredCapabilities();
-        caps.setBrowserName(BrowserType.CHROME);
+        caps.setBrowserName(BrowserType.EDGE);
         try {
             driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), caps);
-            driver.get("http://www.google.com");
+            driver.get("http://www.facebook.com");
             Thread.sleep(20000);
             driver.manage().window().fullscreen();
             System.out.println(" From Remote " + driver.getTitle());
@@ -31,18 +29,11 @@ public class GridRunner extends TestBase{
     }
 
     @Test
-    public void invokeChrome() throws Exception {
-        Thread.sleep(4000);
-        TestBase.initialization("chrome");
-        TestBase.getDriver().get("http://www.google.com");
+    public void invokeIE() throws Exception {
+        TestBase.initialization("ie");
+        TestBase.getDriver().get("http://www.facebook.com");
         Thread.sleep(20000);
         TestBase.getDriver().quit();
     }
-
-    public static void main(String[] args) throws IOException {
-        Runtime.getRuntime().exec("docker-compose -f docker-compose-v3-video.yml up");
-    }
-
-
 
 }
